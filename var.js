491aacd530fc71631d8d1e009f579e0502296057
Cosmin <supercoze@gmail.com>
2024-10-08 17:29:18 +0300

    asd

---
+++ var.js
@@ -0,0 +1,13 @@
+function test() {
+    if(true) {
+        var var1 = 1;
+        let let2 = 2;
+        const const3 = 3;
+    }
+
+    console.log(var1);
+    console.log(let2);
+    console.log(const3);
+}
+
+test();
